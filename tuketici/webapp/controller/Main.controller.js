2660ba9aec0271ab2b231941a9bf31de27b8157f
Kaan Can Calkan <cancalkaniletisim@gmail.com>
2023-03-01 03:10:03 +0300

    Update Main.controller.js

--- tuketici/webapp/controller/Main.controller.js
+++ tuketici/webapp/controller/Main.controller.js
@@ -1,15 +1,25 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
+	"sap/base/Log",
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/Sorter",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/core/format/DateFormat",
+	"sap/m/ToolbarSpacer",
+	"sap/ui/table/library",
+	"sap/ui/thirdparty/jquery"
+], 
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function(Log, Controller, Sorter, JSONModel, DateFormat, ToolbarSpacer, library, jQuery) {
         "use strict";
+        var SortOrder = library.SortOrder;
 
         return Controller.extend("noktasisorgulama.tuketici.controller.Main", {
             onInit: function () {
-
+                var oJSONModel = this.initSampleDataModel();
+                var oView = this.getView();
+                oView.setModel(oJSONModel);
             }
         });
     });
